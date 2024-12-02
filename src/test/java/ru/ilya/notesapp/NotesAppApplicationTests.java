7ebec80c50c31b589550bb368f7f4339c43c28ff
Ilya <rzhunovskiy@gmail.com>
2024-12-02 22:26:23 +0500

    initial commit

---
+++ src/test/java/ru/ilya/notesapp/NotesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ilya.notesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
